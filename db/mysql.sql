24a88075fcccc2673af8d29c68bf162ac70f76c1
Chris <chris@hsweb.us>
2010-07-01 06:14:11 +0000

    Changed the database for the branch.

--- db/mysql.sql
+++ db/mysql.sql
@@ -3,15 +3,131 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 01, 2010 at 06:01 AM
+-- Generation Time: Jul 01, 2010 at 06:12 AM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.2-1ubuntu4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Database: `cakebase`
+-- Database: `cakebackup`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=16 ;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`) VALUES
+(1, 'Administrators'),
+(2, 'Registered users');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups_users`
+--
+
+CREATE TABLE IF NOT EXISTS `groups_users` (
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `group_id` int(10) unsigned NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `groups_users`
+--
+
+INSERT INTO `groups_users` (`user_id`, `group_id`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE IF NOT EXISTS `profiles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `lastname` varchar(50) NOT NULL DEFAULT '',
+  `firstname` varchar(50) NOT NULL DEFAULT '',
+  `about` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `profiles`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles_fields`
+--
+
+CREATE TABLE IF NOT EXISTS `profiles_fields` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `varname` varchar(50) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `field_type` varchar(50) NOT NULL,
+  `field_size` int(3) NOT NULL DEFAULT '0',
+  `field_size_min` int(3) NOT NULL DEFAULT '0',
+  `required` int(1) NOT NULL DEFAULT '0',
+  `match` varchar(255) NOT NULL,
+  `range` varchar(255) NOT NULL,
+  `error_message` varchar(255) NOT NULL,
+  `other_validator` varchar(255) NOT NULL,
+  `default` varchar(255) NOT NULL,
+  `position` int(3) NOT NULL DEFAULT '0',
+  `visible` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `varname` (`varname`,`visible`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `profiles_fields`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rules`
+--
+
+CREATE TABLE IF NOT EXISTS `rules` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Rule description',
+  `group_id` int(10) unsigned DEFAULT NULL,
+  `order` int(10) unsigned DEFAULT NULL,
+  `action` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `permission` enum('Deny','Allow') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Deny',
+  `forward` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `message` varchar(512) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `rules`
+--
+
+INSERT INTO `rules` (`id`, `name`, `group_id`, `order`, `action`, `permission`, `forward`, `message`) VALUES
+(1, 'Allow everything for Administrators', 1, 999999, '*', 'Allow', '', ''),
+(2, 'Allow anybody to see the home page, the error page, to register, to log in, see profile and log out', NULL, 200, '/ or /user/* or /register or /login or /logout or /lost-password or /verify(/)?* or /pass(/)?* or /profile or /denied', 'Allow', '', ''),
+(4, 'Deny everything for everybody by default (allow to have allow by default then deny)', NULL, 0, '*', 'Deny', '', 'Access denied!'),
+(6, 'Display a message for denied admin page', NULL, 100, '(/index)? or /users* or /groups* or /rules*', 'Deny', '', 'You are not allowed to access the administration page!');
 
 -- --------------------------------------------------------
 
@@ -41,5 +157,4 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `email`, `emailcheckcode`, `passwordchangecode`, `disable`, `lastvisit`, `expire_account`, `created`, `updated`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'root', '', '', 0, '2010-07-01 05:59:16', '0000-00-00', '0000-00-00 00:00:00', '2008-02-12 12:19:31');
-
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'root', '', '', 0, '2010-06-30 16:48:48', '0000-00-00', '0000-00-00 00:00:00', '2008-02-12 12:19:31'),
